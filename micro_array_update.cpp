9668b1f008b7275c1f0c8ec8e1a0021cb1594813
Shivam Agarwal <24.shivamagarwal@gmail.com>
2017-10-26 01:10:37 +0530

    Create micro_array_update.cpp

---
+++ micro_array_update.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int test_case_count;
+    cin>>test_case_count;
+    int n,k,i;
+    for(i=0;i<test_case_count;i++){
+        cin>>n;
+        cin>>k;
+        int arr[n],min=1000000;
+        for(int j=0;j<n;j++){
+            cin>>arr[j];
+            if(arr[j]<min)
+                min=arr[j];
+        }
+        int flag=1,c=0;
+            if(min<k)
+                flag=0;
+           while(flag==0){
+                flag=1;
+                min++;
+				if(min<k)
+                    flag=0;
+           		
+               c++;
+           }
+			    
+                cout<<c<<endl;
+    }
+    
+    
+    
+    return 0;
+}
