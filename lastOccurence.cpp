a1657bb464194965d5df176617d5c26d67aed1f0
Shivam Agarwal <24.shivamagarwal@gmail.com>
2017-11-01 23:59:57 +0530

    Update lastOccurence.cpp

--- lastOccurence.cpp
+++ lastOccurence.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 int main()
 {
+  int testCases,numEle;
+  cin>>testCases;
+  cin>>numEle;
+  int arr[numEle];
+  for(int i=0;i<numEle;i++)
+    cin>>arr[i];
+  int numQueries,x;
+  cin>>numQueries;
+  for(int i=0;i>numQueries;i++){
+    
+  
+  }
   return 0;
 }
