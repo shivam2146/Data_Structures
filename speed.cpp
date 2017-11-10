014494cb0a0d872ddf9ec9d8cc39de81c36c37d7
Shivam Agarwal <24.shivamagarwal@gmail.com>
2017-11-10 09:08:59 +0530

    find the count of cars driving with max speed

--- speed.cpp
+++ speed.cpp
@@ -1,14 +1,26 @@
-#include<iostrean>
+#include<iostream>
+//#include<bits/stdc++.h>
 using namespace std;
 int main(){
   int t;
   cin >> t;
   while (t > 0){
-    int n;
+    int n,count=1;
     cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++)
-      cin >> arr[i];
+    cin >> arr[0];
+    int min=arr[0];
+    for (int i = 1;i < n;i++){
+	 cin >> arr[i];
+	 if (arr[i] <= min){
+	 	count++;
+	 	min=arr[i];
+	 }
+	 
+	}
+	cout<<count<<"\n";
+	t--;
+	
   }
   return 0;
 }
