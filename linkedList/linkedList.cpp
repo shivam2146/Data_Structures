463f3d0688df192a52348d51314c5ab09987aa8d
shivam2146 <shivam.mcs17.du@gmail.com>
2018-06-20 21:10:20 +0530

    add linked list and insert at head

---
+++ linkedList/linkedList.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+template <typename T>
+struct node{
+  T data;
+  node *next;
+};
+
+template <typename T>
+class LinkedList{
+  node<T> *head;
+public:
+  LinkedList(){
+    head = NULL;
+  }
+  void insertAtHead(int);
+  void print();
+};
+
+template <typename T>
+void LinkedList<T>::insertAtHead(int key){
+  node<T>*temp = new node<T>;
+  temp->data = key;
+  if(!head){
+    temp->next = NULL;
+    head = temp;
+    temp = NULL;
+    return;
+  }
+  temp->next = head;
+  head = temp;
+  temp = NULL;
+}
+
+template<typename T>
+void LinkedList<T>::print(){
+
+  node<T> *temp = head;
+  cout<<"\ncontent of linked list: ";
+  for(;temp!=NULL;temp= temp->next)
+      cout<<temp->data<<" ";
+}
+
+int main(){
+  LinkedList<int> l;
+  l.insertAtHead(5);
+  l.insertAtHead(1);
+  l.insertAtHead(10);
+  l.insertAtHead(12);
+  l.insertAtHead(63);
+  l.insertAtHead(8);
+  l.print();
+  return 0;
+}
