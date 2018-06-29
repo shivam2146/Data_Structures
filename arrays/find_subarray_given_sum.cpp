2017c9f4970a044084d01cc9a03a4c57e904639a
shivam2146 <shivam.mcs17.du@gmail.com>
2018-06-29 10:33:00 +0530

    add solution using hashing

--- arrays/find_subarray_given_sum.cpp
+++ arrays/find_subarray_given_sum.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
 //sliding window
@@ -16,10 +17,25 @@ void subArrayGivenSum(int *arr,int n,int sum){
   }
 }
 
+//using hashing
+void subArrayGivenSumHash(int *arr,int n,int s){
+  unordered_map <int,int> map;
+  map[0] = -1;
+  int sum= 0;
+  for(int i= 0; i<n; i++){
+    sum += arr[i];
+    if(map.find(sum-s) != map.end()){
+      printf("\nsub array with given sum is :[%d...%d]",map[sum-s]+1,i);
+      return;
+    }
+    map[sum] = i;
+  }
+}
 int main(){
   int arr[]= {2,6,0,9,7,3,1,4,1,10};
   int size= sizeof(arr)/sizeof(arr[0]);
   int sum = 15;
   subArrayGivenSum(arr,size,sum);
+  subArrayGivenSumHash(arr,size,sum);
   return 0;
 }
