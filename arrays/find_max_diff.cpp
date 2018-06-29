4091b9ce22ed7ec6ccde491c0294162a392a4c7f
shivam2146 <shivam.mcs17.du@gmail.com>
2018-06-29 09:53:50 +0530

    O(n) time solun:

--- arrays/find_max_diff.cpp
+++ arrays/find_max_diff.cpp
@@ -18,9 +18,27 @@ void maxDiffPair(int arr[],int n){
   cout<<"maximum difference is : "<<maxdiff<<" and the pair is : ("<<arr[p.first]<<","<<arr[p.second]<<")\n";
 }
 
+int max(int a,int b){
+  return ((a>b)? a: b);
+}
+
+//O(n) time solution
+int maxDiff(int arr[],int n){
+  int maxele = arr[n-1];
+  int maxdiff = INT_MIN;
+  for(int i=n-2; i>=0; i--){
+    if(arr[i]> maxele)
+      maxele = arr[i];
+    else
+    maxdiff = max(maxdiff, maxele-arr[i]);
+  }
+  return maxdiff;
+}
+
 int main(){
   int arr[]={2,7,9,5,1,3,5};
   int n = sizeof(arr)/sizeof(arr[0]);
   maxDiffPair(arr,n);
+  cout<<"\nmax diff is: "<<maxDiff(arr,n);
   return 0;
 }
